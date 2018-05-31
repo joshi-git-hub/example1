d6ab878fe2b41c938946bc2f548fe1b711c81174
Ganesh chandra joshi <37043894+joshi-git-hub@users.noreply.github.com>
2018-05-31 09:01:08 +0530

    in this program user will choose one of them.

--- example.sh
+++ example.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 # is there anybudy who can make shell program.
+# i am writing an example here of shell scripting.
+# in this example i will tell you how to use select loop in scripting.
+# ok, lats begin,
+#!/bin/bash
+select i in tea coffee milk cold_coffee
+do
+echo "Congrates you Have selected :> $i"
+break
+done
